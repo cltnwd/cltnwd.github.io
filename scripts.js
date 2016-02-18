4ab34d060250a4b47476adb908afa45017b93656
cltnwd <coltonhair@gmail.com>
2016-02-18 15:02:09 -0600

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -68,7 +68,7 @@ function loadReddit(sub) {
                     img.src = "http://www.thisiscolossal.com/wp-content/uploads/2015/03/florian-1.gif";
                 }
 
-                img.src = thumb;
+                img.src = exturl;
 
                 cardselector[i].appendChild(img);
 
