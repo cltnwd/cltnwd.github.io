879122f40fadf2a8e8fb859b20eb257b6c387751
cltnwd <coltonhair@gmail.com>
2016-02-18 13:21:33 -0600

    links open in new tab

--- scripts.js
+++ scripts.js
@@ -21,7 +21,7 @@ function loadReddit(sub) {
             // creates a card
             var card = document.createElement("div");
             card.className = "row";
-            card.innerHTML = "<div id='card' class='col-xs-12 card'><h5 class='cardtitle'><a class='link' id='title' href='' style='font-family: helvetica'></a></h5>";
+            card.innerHTML = "<div id='card' class='col-xs-12 card'><h5 class='cardtitle'><a class='link' id='title' href='' target='_blank' style='font-family: helvetica'></a></h5>";
 
             // append card to cardholder
             cardholder.appendChild(card);
