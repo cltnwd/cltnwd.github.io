9be94444456a83371c32acb629df49c42e42f3df
Colton Wood <coltonwood.dev@gmail.com>
2017-04-06 12:35:45 -0500

    Complete Vue rewrite
    rewriting in vue. easier/cleaner.

--- app.js
+++ app.js
@@ -1,89 +1,74 @@
 $(document).ready(function() {
-	var timertext;
-	var timerlink;
-	
-	var typeText = function (target, message, index, interval) {   
 
- 		if (index < message.length) {
- 			if (message[index] == '_') {
- 				$(target).append("<br><br>");
- 				index++;
- 				timertext = setTimeout(function () { 
-    				typeText(target, message, index, interval); 
-    			}, interval);
- 			}
- 			else { 
-    			$(target).append(message[index++]);
-    			timertext = setTimeout(function () { 
-    				typeText(target, message, index, interval); 
-    			}, interval);
-  			}
-  		}
-	}
+    // for "typing" the text(s)
+    var timertext;
+    var timerlink;
 
-	var typeLink = function (target, message, index, interval) {   
+    // type animation for regular text
+    var typeText = function(target, message, index, interval) {
 
- 		if (index < message.length) {
- 			 
-			$(target).html($(target).html() + message[index++]);
-			timerlink = setTimeout(function () { 
-				typeLink(target, message, index, interval); 
-			}, interval);
-  			
-  		}
-	}
+        // still need text to append
+        if (index < message.length) {
 
-// 	typeText("#var", "var ", 0, 500);
-// 	typeText("#varname", "bio;", 0, 500);
+            // adds newline <br>
+            if (message[index] == '_') {
+                $(target).append("<br><br>");
+                index++;
+                timertext = setTimeout(function() {
+                    typeText(target, message, index, interval);
+                }, interval);
+            }
 
-// 	console.log("hello, world");
-	
-	// biography toggle
-	$("#biolink").click(function() {
-		clearTimeout(timertext);
-		// reset
-		if ($("#biolink").hasClass("active")) {
-			$("#bio").html("bio = function() {...};");
-			$("#biocontent").html("");
-			$("#biocontent").toggle();
-			$("#endbio").toggle();			
-		}
-		else {
+            // normal text
+            else {
+                $(target).append("<span class=\"string\">" + message[index++] + "</span>");
+                timertext = setTimeout(function() {
+                    typeText(target, message, index, interval);
+                }, interval);
+            }
+        }
+    }
 
-			$("#bio").html("bio = function() {");
-			$("#biocontent").toggle();
-			$("#endbio").toggle();
-			typeText("#biocontent", "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non facilisis felis. In consectetur ex eget ante venenatis vehicula. Curabitur ut nisi tortor. Nulla posuere lectus leo, eget luctus erat dapibus vitae. Mauris sem enim, commodo eu erat eget, gravida porta velit. Quisque condimentum, ex a malesuada porttitor, nisi nibh mollis sapien, ut semper sem erat quis ex. Quisque a tempus mauris. Vestibulum venenatis cursus orci laoreet mattis. Maecenas fermentum magna quis risus blandit sagittis. Praesent vitae ligula placerat nisi efficitur sagittis. Cras facilisis enim sit amet accumsan eleifend._Donec id ullamcorper orci. Aenean imperdiet eros felis, id imperdiet enim tincidunt sed. Vestibulum dapibus porta arcu elementum laoreet. Quisque eu lectus vitae libero porttitor pretium a a nulla. Cras at nibh auctor, faucibus libero quis, rhoncus ex. Cras lobortis sapien quis est aliquet, id porttitor turpis pharetra. Vestibulum scelerisque erat a diam mollis mattis. Integer et tellus quam. Mauris quis nisl rutrum, facilisis nisi eu, hendrerit neque.", 0, 1);
-		}
+    // NOT USED ! 
+    // type animation for links (a href)
+    var typeLink = function(target, message, index, interval) {
 
-		// toggle active
-		$("#biolink").toggleClass("active");
+        if (index < message.length) {
 
-	});
-	
-	// // toggle resume iframe
-	// $("#reslink").click(function() {
+            $(target).html($(target).html() + message[index++]);
+            timerlink = setTimeout(function() {
+                typeLink(target, message, index, interval);
+            }, interval);
+        }
+    }
 
-	// 	// $("#resume").slideToggle();
+    // biography toggle
+    $("#biolink").click(function() {
 
-	// 	// reset
-	// 	if ($("#reslink").hasClass("active")) {
-	// 		$("#res").html("resume;");
-	// 		$("#rescontent").html("");
-	// 		$("#rescontent").toggle();
-	// 		$("#endres").toggle();			
-	// 	}
-	// 	else {
-	// 		$("#res").html("resume = {");
-	// 		$("#rescontent").toggle();
-	// 		$("#rescontent").html("<a id='areslink' class='code' href='https://drive.google.com/open?id=0B1FXuplRfrlEb1ZoS1FoZVV6Z1k' target='_blank'></a>");
-	// 		$("#endres").toggle();
+        // stops old animation
+        clearTimeout(timertext);
 
-	// 		typeLink("#areslink", "Click here to view resume.pdf", 0, 25);
-	// 	}
+        // reset the bio
+        if ($("#biolink").hasClass("active")) {
+            $("#bio").html("<span id=\"bio\" class=\"code\">bio = <span class=\"var code\">function</span><span class=\"code\">(</span><span class=\"code param\">me</span><span class=\"code\">);</span>");
+            $("#biocontent").html("");
+            $("#biocontent").toggle();
+            $("#endbio").toggle();
+        } else {
 
-	// 	// toggle active
-	// 	$("#reslink").toggleClass("active");
+            // build bio function
+            $("#bio").html("<span id=\"bio\" class=\"code\">bio = <span class=\"var code\">function</span><span class=\"code\">(</span><span class=\"code param\">me</span><span class=\"code\">) {</span>");
+            $("#biocontent").toggle();
+            $("#endbio").toggle();
 
-	// });
+            // underscore represents newline
+            $("#biocontent").html("<span class=\"code\">me = </span>");
+            typeText("#biocontent", "\"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc non facilisis felis. In consectetur ex eget ante venenatis vehicula. Curabitur ut nisi tortor. Nulla posuere lectus leo, eget luctus erat dapibus vitae. Mauris sem enim, commodo eu erat eget, gravida porta velit. Quisque condimentum, ex a malesuada porttitor, nisi nibh mollis sapien, ut semper sem erat quis ex. Quisque a tempus mauris. Vestibulum venenatis cursus orci laoreet mattis. Maecenas fermentum magna quis risus blandit sagittis. Praesent vitae ligula placerat nisi efficitur sagittis. Cras facilisis enim sit amet accumsan eleifend._Donec id ullamcorper orci. Aenean imperdiet eros felis, id imperdiet enim tincidunt sed. Vestibulum dapibus porta arcu elementum laoreet. Quisque eu lectus vitae libero porttitor pretium a a nulla. Cras at nibh auctor, faucibus libero quis, rhoncus ex. Cras lobortis sapien quis est aliquet, id porttitor turpis pharetra. Vestibulum scelerisque erat a diam mollis mattis. Integer et tellus quam. Mauris quis nisl rutrum, facilisis nisi eu, hendrerit neque.\"", 0, 1);
+        }
+
+        // toggle active
+        $("#biolink").toggleClass("active");
+
+    });
+
 });
---
+++ reddit3/app.js
@@ -0,0 +1,33 @@
+"use strict";
+
+var app = new Vue({
+    el: '#app',
+    data: {
+    	panelClass: '',
+    	squeeze: '',
+        message: 'Hello world!',
+        posts: []
+    },
+    methods: {
+        togglePanel: function() {
+	console.log('tog');
+			this.panelClass = (this.panelClass == '') ? 'active' : '';
+			this.squeeze = (this.squeeze == '') ? 'squeeze' : '';
+        },
+        getData: function(rURL) {
+            $.getJSON(rURL, function(data) {
+                if (data.data.children.length > 0) app.handleData(data);
+                else alert('bad');
+            });
+        },
+
+        handleData: function(data) {
+            for (var i in data.data.children)
+                if (data.data.children[i].kind == 't3') app.posts.push(data.data.children[i]);
+            console.log(app.posts);
+        }
+    },
+    created: function() {
+        this.getData('https://www.reddit.com/.json');
+    }
+});
